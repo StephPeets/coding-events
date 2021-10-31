43195853f7cea26ec4badf70a1ab1663c5d9e4c1
Steph Peets <stephanie.peets@gmail.com>
2021-10-31 02:23:27 -0400

    complete Thymeleaf Views Exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,37 +2,25 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 @Controller
-@RequestMapping("events")
 public class EventController {
 
-	private static List<String> events = new ArrayList<>();
+	private static HashMap<String, String > events = new HashMap<>();
 
-	@GetMapping
-	public String displayAllEvents(Model model) {
-		model.addAttribute("title", "All Events");
-		model.addAttribute("events", events);
-		return "events/index";
-	}
+	@GetMapping("/")
+	public String displayAllEvents(Model model){
+		events.put("Menteaship","A fun meetup for connecting with mentors");
+		events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+		events.put("Javascripty", "An imaginary meetup for Javascript developers");
 
-	@GetMapping("create")
-	public String renderCreateEventForm(Model model){
-		model.addAttribute("title", "Create Event");
-		return "events/create";
-	}
-
-	@PostMapping("create")
-	public String createEvent(@RequestParam String eventName){
-		events.add(eventName);
-		return "redirect:";
+		model.addAttribute("title", "Events");
+		model.addAttribute("events", events);;
+		return "index";
 	}
 
 }
